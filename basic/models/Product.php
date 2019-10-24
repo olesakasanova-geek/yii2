fd64d2cf19f1ac445c418508e19f192056dac22a
kasianova olesia <olesakasanova.geek@gmail.com>
2019-10-24 14:56:25 +0300

    Update Product.php

--- basic/models/Product.php
+++ basic/models/Product.php
@@ -2,13 +2,48 @@
 
 namespace app\models;
 
-use yii\base\Model;
+use Yii;
 
-class Product extends Model
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $price
+ * @property int $created_at
+ */
+class Product extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $title;
-    public $price;
-    public $category;
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price', 'created_at'], 'required'],
+            [['created_at'], 'integer'],
+            [['name', 'price'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+            'created_at' => 'Created At',
+        ];
+    }
 }
