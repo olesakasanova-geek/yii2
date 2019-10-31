318759041f4d41a8f70ae2898ef91ed27f19ee2b
kasianova olesia <olesakasanova.geek@gmail.com>
2019-10-31 15:33:41 +0300

    Update ProductController.php

--- basic/controllers/ProductController.php
+++ basic/controllers/ProductController.php
@@ -66,6 +66,7 @@ class ProductController extends Controller
     public function actionCreate()
     {
         $model = new Product();
+        $model->setScenario('create');
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
@@ -86,6 +87,7 @@ class ProductController extends Controller
     public function actionUpdate($id)
     {
         $model = $this->findModel($id);
+        $model->setScenario('update');
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
