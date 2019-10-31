90c1558fc8c44551c41c048509d172136ea461a6
kasianova olesia <olesakasanova.geek@gmail.com>
2019-10-31 15:33:45 +0300

    Update TestController.php

--- basic/controllers/TestController.php
+++ basic/controllers/TestController.php
@@ -2,8 +2,9 @@
 
 namespace app\controllers;
 
-use app\components\TestService;
 use app\models\Product;
+use yii\base\BaseObject;
+use yii\helpers\VarDumper;
 use yii\web\Controller;
 
 
@@ -18,14 +19,32 @@ class TestController extends Controller
      */
     public function actionIndex()
     {
-        $product = new Product([
-            'id' => 1, 'price' => 3500, 'title' => 'in-IZI', 'category' => 'dress'
-        ]);
 
-        //return Yii::$app->test->run();
+        $product = new Product();
+        $data = [
+            'id' => 2,
+            'name' => 'Bunny',
+            'price' => 2500,
+            'category' => 'toy',
+
+        ];
+         $product->id = 1;
+         $product->name = 'in-IZI';
+         $product->price = '3500';
+         $product->created_at = time();
+
+         $product->setAttributes($data);
+
+
+        //return VarDumper::dumpAsString($product->validate(), 4, true);
+        $product->validate();
+        return VarDumper::dumpAsString($product->getAttributes(), 4, true);
+
 
         return $this->render('index',
-            ['var' => 'data', 'product' => $product]
+            [
+                'data' => \Yii::$app->test->run(),
+                'product' => $product]
         );
     }
 }
