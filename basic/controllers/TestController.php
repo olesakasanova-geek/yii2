ed33cdf8058a2f621ddd5a719934ff8e86298ddf
kasianova olesia <olesakasanova.geek@gmail.com>
2019-11-04 15:35:07 +0300

    Update TestController.php

--- basic/controllers/TestController.php
+++ basic/controllers/TestController.php
@@ -2,51 +2,105 @@
 
 namespace app\controllers;
 
+use app\components\TestService;
 use app\models\Product;
-use yii\base\BaseObject;
+use yii\db\Query;
 use yii\helpers\VarDumper;
 use yii\web\Controller;
-
 
 class TestController extends Controller
 {
 
-
-    /**
-     * Displays about page.
-     *
-     * @return string
-     */
     public function actionIndex()
     {
+        $users1 = User::find()->joinWith(User::RELATION_TASKS_CREATOR_ID)->asArray()->all();
+        $title = "Kasianova";
+        return $this->render('index', [
+            'title' => $title,
+            'users' => $users1,
+        ]);
+    }
 
-        $product = new Product();
-        $data = [
-            'id' => 2,
-            'name' => 'Bunny',
-            'price' => 2500,
-            'category' => 'toy',
+    public function actionInsert()
+    {
 
-        ];
-         $product->id = 1;
-         $product->name = 'in-IZI';
-         $product->price = '3500';
-         $product->created_at = time();
-
-         $product->setAttributes($data);
-
-
-        //return VarDumper::dumpAsString($product->validate(), 4, true);
-        $product->validate();
-        return VarDumper::dumpAsString($product->getAttributes(), 4, true);
-
-
-        return $this->render('index',
+        \Yii::$app->db->createCommand()->insert('user',
             [
-                'data' => \Yii::$app->test->run(),
-                'product' => $product]
-        );
+                'username' => 'Ilyin',
+                'password_hash' => '4d43075f14',
+                'auth_key' => 'key_1',
+                'creator_id' => 1,
+                'updater_id' => 2,
+                'created_at' => 1555143750,
+                'updated_at' => 1555143750,
+            ])->execute();
+
+        \Yii::$app->db->createCommand()->insert('user',
+            [
+                'username' => 'Vasiliev',
+                'password_hash' => 'f4e5da32dd',
+                'auth_key' => 'key_2',
+                'creator_id' => 2,
+                'updater_id' => 3,
+                'created_at' => 1555143750,
+                'updated_at' => 1555143750,
+            ])->execute();
+
+
+        \Yii::$app->db->createCommand()->insert('user',
+            [
+                'username' => 'Smirnov',
+                'password_hash' => 'ed585d7a21',
+                'auth_key' => 'key_3',
+                'creator_id' => 3,
+                'updater_id' => 1,
+                'created_at' => 1555143750,
+                'updated_at' => 1555143750,
+            ])->execute();
+
+        \Yii::$app->db->createCommand()->insert('user',
+            [
+                'username' => 'Kasyanov',
+                'password_hash' => '0cf555b2b6',
+                'auth_key' => 'key_4',
+                'creator_id' => 4,
+                'updater_id' => 3,
+                'created_at' => 1555143750,
+                'updated_at' => 1555143750,
+            ])->execute();
+
+        \Yii::$app->db->createCommand()->batchInsert('task',
+            [
+                'title',
+                'description',
+                'creator_id',
+                'updater_id',
+                'created_at',
+                'updated_at'
+            ],
+
+
+            [
+                ['task1', 'first task', 1, 2, 1555143750, 1555143750],
+                ['task2', 'second task', 2, 3, 1555143750, 1555143750],
+                ['task3', 'third task', 3, 1, 1555143750, 1555143750],
+                ['task4', 'fourth task', 4, 3, 1555143750, 1555143750],
+
+            ])->execute();
+    }
+
+    public function actionSelect()
+    {
+        $data = (new Query())->from('user')
+            ->where(['id' => 1])
+            ->all();
+        $data1 = (new Query())->from('user')
+            ->where(['>', 'id', '1'])
+            ->orderBy(['username' => SORT_DESC])
+            ->all();
+        $data2 = (new Query())->from('user')->count('');
+        $data3 = (new Query())->from('task')
+            ->innerJoin('user', 'user.id = task.creator_id')
+            ->all();
     }
 }
-
-
